4e8123abc4f7608da7fb445ad7d26b5c87bce850
yamazoulay <ya5596@bs.amalnet.k12.il>
2018-10-23 14:52:09 +0300

    final version- simple alert Dialogs

--- app/src/main/java/com/example/user/dialogalertyam/MainActivity.java
+++ app/src/main/java/com/example/user/dialogalertyam/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.user.dialogalertyam;
 
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout l1;
     Random rnd = new Random();
     int R1,G1,B1;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -117,5 +121,18 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog ad = adb.create();
         ad.show();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
     }
 }
